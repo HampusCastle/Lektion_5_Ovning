ee0427c9faf1ce3b745483148bcae70a7820d9a1
HampusCastle <Hampuscastle@gmail.com>
2023-09-19 16:49:36 +0200

    Boolean assignment

--- src/Main.java
+++ src/Main.java
@@ -6,22 +6,48 @@ public class Main {
 
 
     public static void main(String[] args) {
-        System.out.println("What is your name?");
-        Scanner sc = new Scanner(System.in);
-        String name = sc.nextLine();
-        System.out.println("Player #1 " + name);
+       // System.out.println("What is your name?");
+       Scanner sc = new Scanner(System.in);
+       // String name = sc.nextLine();
+      //  System.out.println("Player #1 " + name);
 
-        System.out.println("For your next task please select a number");
-        int number = sc.nextInt();
-        for (int i = 0; i < number; i++) {
-            System.out.println(i);
-        }
+      //  System.out.println("For your next task please select a number");
+      //  int number = sc.nextInt();
+      //  for (int i = 0; i < number; i++) {
+       //     System.out.println(i);
+       // }
 
-        do {
-            System.out.println("Give me a number");
-            int newNumber = sc.nextInt();
-            System.out.println("Your number was " + newNumber);
-        }while(true);
+       // do {
+       //     System.out.println("Give me a number");
+       //     int newNumber = sc.nextInt();
+        //    System.out.println("Your number was " + newNumber);
+      //  }while(true);
+
+      boolean isPlaying = (true);
+
+      do {
+          System.out.println("Give me something");
+          String input = sc.nextLine();
+          if(input.equals("Stop")) {
+              System.out.println("That's my queue, bye!");
+              break;   }
+
+          else {
+                  System.out.println("Give me something more");
+              }
+          
+      }  while(true);
+
+
+
+
+
+
+
+
+
+
+
 
 
 
