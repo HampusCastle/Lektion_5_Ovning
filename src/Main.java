0db0c0e377da4e9dfc5c4ee6c2cbc92812c60ee5
HampusCastle <Hampuscastle@gmail.com>
2023-09-19 15:48:44 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+
+
+    public static void main(String[] args) {
+        System.out.println("What is your name?");
+        Scanner sc = new Scanner(System.in);
+        String name = sc.nextLine();
+        System.out.println("Player #1 " + name);
+
+
+
+
+    }
+
+
+
+
+    }
+
